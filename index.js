f6664378d7c3c2798aed3500be1c8c4b5f0c8c83
mfghir <fatemeghafari77@gmail.com>
2023-11-25 22:57:01 -0800

    showing product

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
 import { getCookie } from "./utils/cookie.js";
 import { getData } from "./utils/httpReq.js";
+import { shortenText } from "./utils/stringFunc.js";
 
 const loginButton = document.getElementById("login");
 const dashboardButton = document.getElementById("dashboard");
+const mainContent = document.getElementById("products");
 
-const showProducts = (products) => {};
+const showProducts = (products) => {
+  mainContent.innerHTML = "";
+  products.forEach((product) => {
+    const jsx = `
+    <div>
+        <img src=${product.image} alt=${product.title} />
+        <h4>${shortenText(product.title)}</h4>
+        <div id="price">
+            <p>$ ${product.price}</p>
+            <button>
+                Buy
+                <i class="fa-solid fa-cart-shopping"></i>            
+            </button>
+            </div>
+        <div id="rate">
+            <i class="fa-solid fa-star"></i>            
+            <span>${product.rating.rate}</span>
+        </div>
+        <div id="rate">
+            <i class="fa-solid fa-user"></i>            
+            <span>${product.rating.count}</span>
+        </div>
+
+    </div>
+    `;
+
+    mainContent.innerHTML += jsx;
+  });
+};
 
 const init = async () => {
   const cookie = getCookie();
